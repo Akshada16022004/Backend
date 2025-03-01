32ac313be8b880733451b3831c9bfc0c35541e55
unknown <akshadashirke3@example.com>
2025-03-01 18:29:37 +0530

    initial commit

---
+++ Server.js
@@ -0,0 +1,61 @@
+const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+const User=require('./models/User')
+const bcrypt=require('bcryptjs')
+
+
+const app=express()
+const PORT=3000
+app.use(express.json());
+
+app.use("/recipe",require("./routes/Recipe"))
+
+//Registration page api
+
+app.post('/register',async(req, res)=>{
+    const {username,email,password}=req.body
+    try{
+        const hashedPassword= await bcrypt.hash(password,10)
+        const user=new User({username,email,password:hashedPassword})
+        await user.save()
+        res.json({message: "User Registred.."})
+        console.log("User Registration completed...")
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+
+//Login page api
+
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body
+    try{
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) 
+            {
+             return res.status(400).json({ message: "Invalid Credentials" });
+            }
+          res.json({ message: "Login Successful", username: user.username });
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+
+mongoose.connect("mongodb+srv://akshadas1602:iYIT5vZWDgPcNTsn@cluster1.drut4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1").then(
+    ()=>console.log("DB connected successfully..")
+).catch(
+    (err)=>console.log(err)
+)
+
+app.listen(PORT,(err)=>{
+    if(err)
+    {
+        console.log(err)
+    }
+    console.log("Server is running on port :"+PORT)
+})
---
+++ routes/Recipe.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const { getRecipes,getRecipe,addRecipe,editRecipe,deleteRecipe} = require('../controller/Recipe')
+const router=express.Router()
+
+
+router.get('/',getRecipes) //get all recipes
+router.get('/:id',getRecipe) //get recipe by id
+router.post('/',addRecipe) //add recipe
+router.put('/:id',editRecipe) //edit recipe
+router.delete('/:id',deleteRecipe) //delete recipe
+
+
+module.exports=router
